a5419fff1a976629907a24401e25ccfdde22cd94
nhglud <nhglud@gmail.com>
2025-08-17 15:37:41 +0200

    camera class start

---
+++ Camera.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Mathematics;
+
+namespace LearnOpenTK
+{
+    public class Camera
+    {
+        Transform transform;
+
+        public Camera(Transform transform)
+        {
+            this.transform = transform;
+        }
+
+        public Matrix4 GetViewMatrix()
+        {
+            return Matrix4.Identity;
+
+        }
+
+        public Matrix4 GetProjectionMatrix(float aspectRatio)
+        {
+            return Matrix4.Identity;
+        }
+
+    }
+}
